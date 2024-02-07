14c9204da90a150388618c99f24f06124f1314ee
Aristides Gonzalez <aristides@glezpol.com>
2024-02-07 14:19:21 -0500

    chore: simplified aws cmd description

--- cmd/aws.go
+++ cmd/aws.go
@@ -67,12 +67,6 @@ var (
 	awsCmd    = &cobra.Command{
 		Use:   "aws",
 		Short: "Entrypoint for all AWS interactions",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return describeAccount(accountID)
 		},
